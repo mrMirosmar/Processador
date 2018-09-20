1665cdab1bd2bed60a69f4bdb393f0e6d0add538
Mirosmar José <mirosmar2014.1@gmail.com>
2018-09-20 10:44:53 -0300

    Banco de Registradores
    Criação da Classe e Implementação de Variáveis com métodos get() e set().

---
+++ src/BancoRegistradores.java
@@ -0,0 +1,63 @@
+
+public class BancoRegistradores {
+    
+    
+   private int pc;
+   private int mar;
+   private int acc;
+   private String mbr; 
+   private String ir;
+
+    public int getPc() {
+        return pc;
+    }
+
+    public void setPc(int pc) {
+        this.pc = pc;
+    }
+
+    public int getMar() {
+        return mar;
+    }
+
+    public void setMar(int mar) {
+        this.mar = mar;
+    }
+
+    public int getAcc() {
+        return acc;
+    }
+
+    public void setAcc(int acc) {
+        this.acc = acc;
+    }
+
+    public String getMbr() {
+        return mbr;
+    }
+
+    public void setMbr(String mbr) {
+        this.mbr = mbr;
+    }
+
+    public String getIr() {
+        return ir;
+    }
+
+    public void setIr(String ir) {
+        this.ir = ir;
+    }
+   
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
