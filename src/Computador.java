60ff838dd7d6c4e0a82a58fdb779fd28aa72c11f
Mirosmar José <mirosmar2014.1@gmail.com>
2018-10-02 22:02:36 -0300

    Implementação de código.
    uso do método setDados da classe Processador, setEnd da classe Memoria.
    Criação de um laço para ler vetor retorno que pega dados do método buscar2 da classe Processador.

--- src/Computador.java
+++ src/Computador.java
@@ -6,14 +6,33 @@ public class Computador {
     Memoria mem = new Memoria();
     
     
-    
+       /*for(int i=0; i < mem.retornarMemoria().length; i++){
+           System.out.println(mem.retornarMemoria()[i]); 
+       }*/
 
     
-    
+        
+        mem.setEnd(pro.getEnd());
+        pro.setDados(mem.getMemoria()[mem.getEnd()]);
+        String [] retorno = pro.buscar2(mem.getMemoria()[mem.getEnd()]);
+        for(int i=0; i < retorno.length; i++){
+            System.out.println(retorno[i]);
+        }
+        
+        //System.out.println(mem.retornarMemoria()[mem.getEndereco()]);
+        
+        
+        
+        
+        
+        
+        
+        
+        
     /*
     
     pro.buscar();
-    mem.setEndereco(pro.getEnd());
+    
     pro.setDados(mem.getDados());
     pro.buscar2();
     pro.dec();
