2f5cb01abb02e3e233fcd3ac9906683c778c2210
Mirosmar José <mirosmar2014.1@gmail.com>
2018-10-02 21:51:40 -0300

    Novos métodos foram requisitados, e o código comentado.

--- src/Computador.java
+++ src/Computador.java
@@ -6,8 +6,26 @@ public class Computador {
     Memoria mem = new Memoria();
     
     
-    System.out.println(pro.retornarDados());
-        System.out.println(pro.retornarDecodificacao()[0]+"\n"+pro.retornarDecodificacao()[1]);
+    
+
+    
+    
+    /*
+    
+    pro.buscar();
+    mem.setEndereco(pro.getEnd());
+    pro.setDados(mem.getDados());
+    pro.buscar2();
+    pro.dec();
+    pro.buscarDados();
+    mem.setEnd(pro.getEnd());
+    pro.setDados(mem.getDados());
+    */
+    
+        
+        
+        
+        
     /* mem.setEndereco(pro.getMar());
     pro.setMBR(mem.getDados());
     pro.buscar2();*/
