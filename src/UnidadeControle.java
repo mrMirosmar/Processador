52ce32ee663e1e8456860ffa38e021e2a985c614
Mirosmar José <mirosmar2014.1@gmail.com>
2018-10-02 22:53:41 -0300

    Adição e modificação de código da classe UnidadeControle.
    Mudança no nome e tipo de retorno do método buscar para buscarEnd.
    Criaçao e implementação do método interpretar.

--- src/UnidadeControle.java
+++ src/UnidadeControle.java
@@ -1,7 +1,8 @@
-public class UnidadeControle {
 
-   private BancoRegistradores br;
-   
+public class UnidadeControle{
+    
+    private BancoRegistradores br;
+
     public UnidadeControle() {
         br = new BancoRegistradores();
     }
@@ -13,46 +14,29 @@ public class UnidadeControle {
     public void setBr(BancoRegistradores br) {
         this.br = br;
     }
-     
-    //FUNÇÃO BUSCAR
-    public String buscar(){
+    
+    
+    //Pegar o que tá em PC coloca em MAR, e incrementa o pc.
+    public int buscarEnd(){
         br.setMar(br.getPc());
         br.setPc(br.getPc()+1);
-        //br.setMbr(mem.retornarMemoria()[br.getMar()]);
-        br.setIr(br.getMbr());
+        return br.getMar(); 
+    }
+    
+    //Pega a istrunção passada e verifica
+    public String interpretar(String ins){
+        br.setMbr(ins);
+        br.setIr(ins);
         return br.getIr();
     }
     
-    //FUNÇÃO DECODIFICAR
-    public String[] decodificar(String instrucao){
-        String[] decodicacao = instrucao.split(" ");
-        return decodicacao;
+    //Pós interpretação, é gerada a decodificação na istrunção
+    public String[] dec(String ins ){
+        String[] d = ins.split(" ");
+        return d;
     }
     
-    //FUNÇÃO CARREGAR
-    public void carregar(String[] memoria, int endereco){
-       System.out.println("CARREGANDO...");
-       br.setMar(endereco);
-       br.setMbr(memoria[br.getMar()]);
-       br.setAcc(Integer.valueOf(br.getMbr()));
-    }
-    
-    //FUNÇÃO SALVAR
-    public void salvar(String[] memoria, int endereco){
-        System.out.println("SALVANDO...");
-        br.setMar(endereco);
-        memoria[br.getMar()] = String.valueOf(br.getAcc());
-    }
-    
-    public BancoRegistradores retornarObj(){
-        return br;
-    }
-    
-} 
     
     
     
-    
-    
-    
-    
+}
